f9f6f36b3a44c25b26e5b03e9609f1f3b75a1fcc
Jed Northridge <northridge@gmail.com>
2013-08-11 13:58:59 -0400

    [spec] Remove (now) duplicated "DSL" stuff from carry_all

--- spec/lib/european/carry_all_spec.rb
+++ spec/lib/european/carry_all_spec.rb
@@ -22,57 +22,16 @@ module European
         CarryAll.load_file file
       end
 
-      let :projects do
-        carry_all.projects
+      it 'has #projects' do
+        carry_all.should respond_to :projects
       end
 
-      let :reveal_ck do
-        projects['reveal-ck']
+      it 'has #build_systems' do
+        carry_all.should respond_to :build_systems
       end
 
-      let :travis do
-        carry_all.build_systems['travis/jedcn']
-      end
-
-      let :known_project_names do
-        ['reveal-ck', 'reveal-ck-template', 'rake-to-web', 'emacs-setup']
-      end
-
-      describe '#projects' do
-
-        it 'returns a map of European::Projects in the CarryAll' do
-
-          expect(projects.keys).to match_array known_project_names
-
-          carry_all.projects.values.each do |project|
-            expect(project.class).to eq European::Project
-          end
-
-        end
-
-      end
-
-      it 'knits projects, source systems, build systems, and builds together' do
-        # reveal_ck's build system has been set to travis
-        expect(reveal_ck.build_system).to eq travis
-
-        # travis has been told of reveal_ck
-        expect(travis.projects.values).to include reveal_ck
-
-        # reveal_ck has a single build
-        reveal_ck_build = reveal_ck.builds['reveal-ck']
-        expect(reveal_ck.builds.size).to eq 1
-
-        # The build's name has been set
-        expect(reveal_ck_build.name).to eq 'reveal-ck'
-        # The build is aware of the project that it belongs to
-        expect(reveal_ck_build.project).to eq reveal_ck
-        # The build is aware of the build system that it belongs to
-        expect(reveal_ck_build.build_system).to eq travis
-
-        # travis knows about the build
-        expect(travis.builds['reveal-ck']).to eq reveal_ck_build
-        expect(reveal_ck.src_url).to eq 'https://github.com/jedcn/reveal-ck.git'
+      it 'has #source_systems' do
+        carry_all.should respond_to :source_systems
       end
 
     end
